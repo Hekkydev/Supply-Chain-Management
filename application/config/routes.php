2259f56cb4570b2c6ce61fd9c97658cbc8350ff4
Hekkydev <nhekky@gmail.com>
2017-04-05 00:38:42 +0700

    merubah posisi default load

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-$route['default_controller'] = 'Welcome';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
