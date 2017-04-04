f4b674a4947dccf93b105a7fdbb8ab3d2c57970f
Hekkydev <nhekky@gmail.com>
2017-04-05 01:22:53 +0700

    rename file

--- application/config/config_app.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-//  CONFIG APP BY HEKKYDEV REPOSITORY
-
-
-$config['ci_app_title'] = "SCM | Pertamina LPG Sistem Management";
