8bd7ee90e5178021085eb91b3a00f617f3dcfc25
Hekkydev <nhekky@gmail.com>
2017-04-05 01:50:23 +0700

    remove yang gak penting

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -32,19 +32,7 @@
                                         <small>Member since Nov. 2012</small>
                                     </p>
                                 </li>
-                                <!-- Menu Body -->
-                                <li class="user-body">
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Followers</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Sales</a>
-                                    </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Friends</a>
-                                    </div>
-                                </li>
-                                <!-- Menu Footer-->
+                              
                                 <li class="user-footer">
                                     <div class="pull-left">
                                         <a href="#" class="btn btn-default btn-flat">Profile</a>
