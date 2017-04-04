e5dc8930f1973f6a713b4ab639ad5e1f3aeb1f85
Hekkydev <nhekky@gmail.com>
2017-04-04 23:47:14 +0700

    merubah konidisi base url

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://ci3.hmvc';
+$config['base_url'] = 'http://localhost/lpg-app/';
 
 /*
 |--------------------------------------------------------------------------
