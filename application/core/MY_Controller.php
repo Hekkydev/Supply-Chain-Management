ae9617a62b6eb6105c7bfd95d4830818caf8130c
Hekkydev <nhekky@gmail.com>
2017-04-04 23:58:54 +0700

    membuat funggsi load theme untuk tampilan

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,12 +9,20 @@ class MY_Controller extends CI_Controller
     protected $helper = array('url','form');
     protected $library = array('session');
 
-    
+
     function __construct()
     {
         parent::__construct();
         $this->load->database();
         $this->load->helper($this->helper);
         $this->load->library($this->library);
+    }
+
+
+    public function load_theme($content,$data = null)
+    {
+        $this->data['app_title']    = $this->config->item('ci_app_title');
+        $this->data['content']      = $this->load->view($content,$data,TRUE);
+        $this->load->view("template_base/content",$data);
     }
 }
