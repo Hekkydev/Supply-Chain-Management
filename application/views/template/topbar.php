34a9bed172d6b9fe83650ad1b2a55dd187db2639
Hekkydev <nhekky@gmail.com>
2017-04-05 01:41:54 +0700

    create app tite logo

--- application/views/template/topbar.php
+++ application/views/template/topbar.php
@@ -4,7 +4,7 @@
     <div class="wrapper">
 
         <header class="main-header">
-            <a href="#" class="logo"><b>Admin</b>LTE</a>
+            <a href="#" class="logo"><?php echo $app_title_logo; ?></a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top" role="navigation">
                 <!-- Sidebar toggle button-->
