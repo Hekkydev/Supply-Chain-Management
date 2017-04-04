7ad2f8e400a5d083e4ec29ecb748717146bd4d5c
Hekkydev <nhekky@gmail.com>
2017-04-05 01:23:15 +0700

    loadied config data

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,7 +8,7 @@ class MY_Controller extends CI_Controller
     
     protected $helper = array('url','form');
     protected $library = array('session','scm_library');
-
+    protected $configurasi = "config-app";
 
     function __construct()
     {
@@ -16,6 +16,8 @@ class MY_Controller extends CI_Controller
         $this->load->database();
         $this->load->helper($this->helper);
         $this->load->library($this->library);
+        $this->load->config($this->configurasi);
+       
 
     }
 
@@ -24,6 +26,6 @@ class MY_Controller extends CI_Controller
     {
         $this->data['app_title']    = $this->config->item('ci_app_title');
         $this->data['content']      = $this->load->view($content,$data,TRUE);
-        $this->load->view("template/content",$data);
+        $this->load->view("template/content",$this->data);
     }
 }
