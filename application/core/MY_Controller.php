ecb90177172e2aef5b0e49fa18f4664482eaa1b4
Hekkydev <nhekky@gmail.com>
2017-04-04 23:46:48 +0700

    - ubah pewarisan pada my_controller - penambahan library session

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -6,11 +6,15 @@
 class MY_Controller extends CI_Controller
 {
     
-    private $helper = array('url','form');
+    protected $helper = array('url','form');
+    protected $library = array('session');
+
+    
     function __construct()
     {
         parent::__construct();
         $this->load->database();
         $this->load->helper($this->helper);
+        $this->load->library($this->library);
     }
 }
