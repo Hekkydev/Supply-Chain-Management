8f506434578c4a02de9c211265cfe480f17255ec
Hekkydev <nhekky@gmail.com>
2017-04-05 00:20:46 +0700

    membuar library Supply Chain Management

---
+++ application/libraries/SCM_library.php
@@ -0,0 +1,20 @@
+<?php
+defined("BASEPATH") or exit("Jangan Sembarangan Hacking");
+
+/**
+ * SCM Library bu Hekky Nurhikmat 
+ *  general library untuk aplikasi supply chain management
+ *
+ */
+class SCM_library
+{
+    var $SCM;
+
+    function __construct()
+    {
+        $this->SCM =& get_instance();
+    }
+
+
+
+}
