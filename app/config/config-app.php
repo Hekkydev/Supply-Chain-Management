675a7d7da63630f560b365597c3308eae8bf5acb
= <=>
2017-09-30 11:36:05 +0700

    update title apps

--- app/config/config-app.php
+++ app/config/config-app.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //  CONFIG APP BY HEKKYDEV REPOSITORY
 
 
-$config['ci_app_title'] = "SCM | Pertamina LPG Sistem Management";
+$config['ci_app_title'] = "PT. MGP | Agen LPG - Supply Chain Management";
 
 $config['ci_app_title_logo'] = "SCM APP";
 $config['nomor-pembayaran-invoice'] = 'BCA - 5676517326';
